864e28de7063379946bc89cc82e791eafcb75e48
Campbell Glass <campbelltmglass@gmail.com>
2015-10-18 21:15:41 -0700

    Fixed indentation formatting

--- Datum.java
+++ Datum.java
@@ -5,63 +5,63 @@ package cartograph;
  */
 public class Datum implements Comparable<Datum> {
 
-  private final Point p; // the point in space in question
-  private final double elev; // the elevation of the point in m
+	private final Point p; // the point in space in question
+	private final double elev; // the elevation of the point in m
 
-  public Datum(Point p, double elev) {
-    this.p = new Point(p.getX(), p.getY()); // copy-in
-    this.elev = elev;
-  } 
+	public Datum(Point p, double elev) {
+		this.p = new Point(p.getX(), p.getY()); // copy-in
+		this.elev = elev;
+	} 
 
-  public Datum(int x, int y, double elev) {
-    this(new Point(x, y), elev);
-  }
+	public Datum(int x, int y, double elev) {
+		this(new Point(x, y), elev);
+	}
 
-  /*
-   * Returns the Point backing this Datum
-   */
-  public Point getPoint() {
-    return new Point(this.p.getX(), this.p.getY()); // copy-out
-  }
+	/*
+	 * Returns the Point backing this Datum
+	 */
+	public Point getPoint() {
+		return new Point(this.p.getX(), this.p.getY()); // copy-out
+	}
 
-  /*
-   * Returns the x-coordinate of this Datum
-   */
-  public int getX() {
-    return this.getPoint().getX();
-  }
+	/*
+	 * Returns the x-coordinate of this Datum
+	 */
+	public int getX() {
+		return this.getPoint().getX();
+	}
 
-  /*
-   * Returns the y-coordinate of this Datum
-   */
-  public int getY() {
-    return this.getPoint().getY();
-  }
+	/*
+	 * Returns the y-coordinate of this Datum
+	 */
+	public int getY() {
+		return this.getPoint().getY();
+	}
 
-  /*
-   * Returns the elevation of this Datum
-   */
-  public double getElev() {
-    return this.elev;
-  }
+	/*
+	 * Returns the elevation of this Datum
+	 */
+	public double getElev() {
+		return this.elev;
+	}
 
-  /*
-   * Equality for Datum objects is defined solely by their Points
-   */
-  public boolean isEqual(Object o) {
-    if (!(o instanceof Datum)) {
-      return false;
-    }
-    Datum other = (Datum) o;
-    return this.getPoint().isEqual(other.getPoint());
-  }
+	/*
+	 * Equality for Datum objects is defined solely by their Points
+	 */
+	public boolean isEqual(Object o) {
+		if (!(o instanceof Datum)) {
+			return false;
+		}
+		Datum other = (Datum) o;
+		return this.getPoint().isEqual(other.getPoint());
+	}
 
-  /*
-   * Comparison for Datum objects is defined solely by their Points
-   */
-  public int compareTo(Datum other) {
-    return this.getPoint().compareTo(other.getPoint());
-  }
+	/*
+	 * Comparison for Datum objects is defined solely by their Points
+	 */
+	public int compareTo(Datum other) {
+		return this.getPoint().compareTo(other.getPoint());
+	}
 }
 
 
--- Point.java
+++ Point.java
@@ -4,64 +4,64 @@ package cartograph;
  * Represents an immutable point in 2-dimensional space
  */
 public class Point implements Comparable<Point> {
-  
-  // 2-d coordinates of this point
-  private final int x;
-  private final int y;
 
-  public Point(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+	// 2-d coordinates of this point
+	private final int x;
+	private final int y;
 
-  /**
-   * Gets the x-coordinate of this point
-   */
-  public int getX() {
-    return this.x;
-  }
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-  /**
-   * Gets the y-coordinate of this point
-   */
-  public int getY() {
-    return this.y;
-  }
+	/**
+	 * Gets the x-coordinate of this point
+	 */
+	public int getX() {
+		return this.x;
+	}
 
-  /**
-   * Returns true if this point is equal to the other point.
-   * Equality for points is defined as occupying the same point in space.
-   */
-  public boolean isEqual(Object o) {
-    if (!(o instanceof Point)) {
-      return false;
-    }
-    Point other = (Point) o;
-    return this.x == other.x && this.y == other.y;
-  }
+	/**
+	 * Gets the y-coordinate of this point
+	 */
+	public int getY() {
+		return this.y;
+	}
 
-  /**
-   * Returns negative if this point is to the right of other,
-   * or if this point is equally right but below other
-   * Returns positive if this point is to the left of other,
-   * or if this point is equally right but above other.
-   * Returns 0 if this point is equal to other
-   */
-  public int compareTo(Point other) {
-    if (this.x != other.x) {
-      return other.x - this.x;
-    }
-    return other.y - this.y;
-  }
+	/**
+	 * Returns true if this point is equal to the other point.
+	 * Equality for points is defined as occupying the same point in space.
+	 */
+	public boolean isEqual(Object o) {
+		if (!(o instanceof Point)) {
+			return false;
+		}
+		Point other = (Point) o;
+		return this.x == other.x && this.y == other.y;
+	}
 
-  /**
-   * Returns the pythagorean distance from this point to the other point
-   */
-  public double distanceTo(Point other) {
-    double dX = Math.pow(this.getX() - other.getX(), 2.0);
-    double dY = Math.pow(this.getY() - other.getY(), 2.0);
-    return Math.pow(dX + dY, 0.5);
-  }
+	/**
+	 * Returns negative if this point is to the right of other,
+	 * or if this point is equally right but below other
+	 * Returns positive if this point is to the left of other,
+	 * or if this point is equally right but above other.
+	 * Returns 0 if this point is equal to other
+	 */
+	public int compareTo(Point other) {
+		if (this.x != other.x) {
+			return other.x - this.x;
+		}
+		return other.y - this.y;
+	}
+
+	/**
+	 * Returns the pythagorean distance from this point to the other point
+	 */
+	public double distanceTo(Point other) {
+		double dX = Math.pow(this.getX() - other.getX(), 2.0);
+		double dY = Math.pow(this.getY() - other.getY(), 2.0);
+		return Math.pow(dX + dY, 0.5);
+	}
 }
 
 
